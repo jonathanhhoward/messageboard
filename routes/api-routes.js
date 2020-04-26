c7554fb51637fd7fc6033dc215c75d72ebdfb8b1
jonathanhhoward <2773262@gmail.com>
2020-04-25 23:19:36 -0400

    Initial commit from boilerplate

---
+++ routes/api-routes.js
@@ -0,0 +1,17 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict'
+
+const router = require('express').Router()
+
+router.route('/threads/:board')
+
+router.route('/replies/:board')
+
+module.exports = router
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,54 +6,54 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const assert = require('chai').assert
+const server = require('../server')
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
-    
+
     suite('POST', function() {
-      
+
     });
-    
+
     suite('GET', function() {
-      
+
     });
-    
+
     suite('DELETE', function() {
-      
+
     });
-    
+
     suite('PUT', function() {
-      
+
     });
-    
+
 
   });
-  
+
   suite('API ROUTING FOR /api/replies/:board', function() {
-    
+
     suite('POST', function() {
-      
+
     });
-    
+
     suite('GET', function() {
-      
+
     });
-    
+
     suite('PUT', function() {
-      
+
     });
-    
+
     suite('DELETE', function() {
-      
+
     });
-    
+
   });
 
 });
