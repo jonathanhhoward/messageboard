4428542e1439137c521c3fd42e84f7e92fd7bfae
jonathanhhoward <2773262@gmail.com>
2020-04-26 14:44:05 -0400

    Initial commit from boilerplate

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -10,7 +12,7 @@ const runner = require('./test-runner')
 
 const app = express()
 
-app.use('/public', express.static(process.cwd() + '/public'))
+app.use('/public', express.static(`${process.cwd()}/public`))
 
 app.use(cors({ origin: '*' })) //For FCC testing purposes only
 
@@ -18,20 +20,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html')
-  })
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html')
-  })
+app.get('/b/:board/', (req, res) => res.sendFile(`${process.cwd()}/views/board.html`))
+app.get('/b/:board/:threadid', (req, res) => res.sendFile(`${process.cwd()}/views/thread.html`))
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html')
-  })
+app.get('/', (req, res) => res.sendFile(`${process.cwd()}/views/index.html`))
 
 //For FCC testing purposes
 fccTestingRoutes(app)
@@ -40,22 +33,18 @@ fccTestingRoutes(app)
 app.use('/api', apiRoutes)
 
 //404 Not Found Middleware
-app.use(function (req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found')
-})
+app.use((req, res, next) => res.status(404).send('Not Found'))
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on port ' + process.env.PORT)
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log('Listening on port', port)
   if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...')
     setTimeout(function () {
       try {
         runner.run()
-      } catch (e) {
-        var error = e
+      } catch (error) {
         console.log('Tests are not valid:')
         console.log(error)
       }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,52 +8,45 @@
 
 const chaiHttp = require('chai-http')
 const chai = require('chai')
-const assert = require('chai').assert
 const server = require('../server')
 
-chai.use(chaiHttp);
+const assert = chai.assert
+chai.use(chaiHttp)
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+  suite('API ROUTING FOR /api/threads/:board', function () {
+    suite('POST', function () {
 
-  suite('API ROUTING FOR /api/threads/:board', function() {
+    })
 
-    suite('POST', function() {
+    suite('GET', function () {
 
-    });
+    })
 
-    suite('GET', function() {
+    suite('DELETE', function () {
 
-    });
+    })
 
-    suite('DELETE', function() {
+    suite('PUT', function () {
 
-    });
+    })
+  })
 
-    suite('PUT', function() {
+  suite('API ROUTING FOR /api/replies/:board', function () {
+    suite('POST', function () {
 
-    });
+    })
 
+    suite('GET', function () {
 
-  });
+    })
 
-  suite('API ROUTING FOR /api/replies/:board', function() {
+    suite('PUT', function () {
 
-    suite('POST', function() {
+    })
 
-    });
+    suite('DELETE', function () {
 
-    suite('GET', function() {
-
-    });
-
-    suite('PUT', function() {
-
-    });
-
-    suite('DELETE', function() {
-
-    });
-
-  });
-
-});
+    })
+  })
+})
