ae8fd64cd0382f3924f30b5d8f3642e37bd2c3c8
jonathanhhoward <2773262@gmail.com>
2020-04-26 14:46:48 -0400

    Add helmet with referrerPolicy 'same-origin'

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const helmet = require('helmet')
 
 const apiRoutes = require('./routes/api-routes.js')
 const fccTestingRoutes = require('./routes/fcctesting.js')
 const runner = require('./test-runner')
 
 const app = express()
+
+app.use(helmet({
+  referrerPolicy: { policy: 'same-origin' }
+}))
 
 app.use('/public', express.static(`${process.cwd()}/public`))
 
