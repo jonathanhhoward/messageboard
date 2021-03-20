8aa82f1e3da7608b5b93b6cda6651635eab18d3f
Jonathan Howard <2773262@gmail.com>
2021-03-19 20:51:04 -0400

    remove test for res.body.length

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -39,13 +39,11 @@ suite("Functional Tests", function () {
 
             assert.isArray(res.body);
             assert.isAtMost(res.body.length, 10);
-            if (res.body.length > 1) {
-              for (let i = 0; i < res.body.length - 1; ++i) {
-                assert.isBelow(
-                  Date.parse(res.body[i + 1].bumped_on),
-                  Date.parse(res.body[i].bumped_on)
-                );
-              }
+            for (let i = 0; i < res.body.length - 1; ++i) {
+              assert.isBelow(
+                Date.parse(res.body[i + 1].bumped_on),
+                Date.parse(res.body[i].bumped_on)
+              );
             }
 
             assert.isObject(res.body[0]);
