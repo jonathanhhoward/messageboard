d247ba6c9e7f4cd1bd06ffdccb5f694b02d8cd08
Jonathan Howard <2773262@gmail.com>
2021-03-25 01:29:58 -0500

    use Thread.remove()

--- db/clear.js
+++ db/clear.js
@@ -1,9 +1,11 @@
-const { connection } = require("./index");
+const Thread = require("../model");
 
 module.exports = async function () {
   try {
-    await connection.dropCollection("test");
-    await connection.dropCollection("replies");
+    const threads = await Thread.Thread("test").find();
+    for (const thread of threads) {
+      await Thread.remove("test", thread._id, "thread password");
+    }
     console.log("[Clear]: success!");
   } catch (err) {
     console.error("[MongoDB]:", err);
